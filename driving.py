3bae1cdc130a072443fe75068a8e8cc3200b291f
Leo Lee <yichenglee_leo@outlook.com>
2018-10-06 10:29:03 +0800

    modification

--- driving.py
+++ driving.py
@@ -1,15 +1,16 @@
 country = input('What is your nationality? ')
-age = input('What is your age? ')
-age = int(age)
 
 if country == 'Taiwan':
-	if age >= 18:
+	age = input('What is your age? ')
+	if int(age) >= 18:
 		print('You could have licence test.')
 	else:
 		print('You could NOT have licence test.')
-
 elif country == 'US':
-	if age >= 16:
+	age = input('What is your age? ')
+	if int(age) >= 16:
 		print('You could have licence test.')
 	else:
 		print('You could NOT have licence test.')
+else:
+	print('You should only fill in your nationality as Taiwan or US.')
